ddcb3400c32ddc1f300f563d5a0de93107f1693a
Irene <irenemanningm@gmail.com>
2022-05-24 21:48:15 -0500

    commit tree 091c322c3f9fe56a7024f13049b6a5aa1feaaae2

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add x, y
+    return x + y
+end
+
+def halve x
+    if x.class == String
+        return nil
+    end
+    return x / 2
+end
